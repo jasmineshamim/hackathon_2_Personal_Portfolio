ef6548dc2fc6f8e7f41ec550befe5d7cd4732406
jasmine sheikh <169073734+jasmineshamim@users.noreply.github.com>
2024-11-10 16:07:07 +0500

    Add files via upload

---
+++ tailwind.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      transitionTimingFunction:{
+        'jump': 'cubic-bezier(0.68, -0.55, 0.265,1.55)'
+      },
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
